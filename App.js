ecff5eba1f01d9833b4697cd59b0cae5b582e053
Laurenz Dilba <laurenz.dilba@hotmail.de>
2021-04-03 14:28:15 +0200

    Bootstrap Layouting

--- App.js
+++ App.js
@@ -6,16 +6,16 @@ var path = require("path");
 const hostname = '127.0.0.1';
 const port = 3000;
 
-var html;
-fs.readFile("html/index.html", (err,data) => {html = data})
+// var html;
+// fs.readFile("./test.html", (err,data) => {html = data})
 
 var app = express();
 app.use(express.static(path.join(__dirname, '/')));
 
 
 app.get('/',(req,res) =>{
-
-    res.write(html)
+    // console.log("Hallo")
+    // res.write("HAllol")
     res.send()
 
 })
